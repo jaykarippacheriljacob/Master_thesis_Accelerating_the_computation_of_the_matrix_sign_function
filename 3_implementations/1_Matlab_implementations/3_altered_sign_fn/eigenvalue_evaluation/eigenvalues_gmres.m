10d1c0709807e56d8a96d710716270f3388fa128
Jay Karippacheril Jacob <jaykarippacheriljacob@gmail.com>
2024-11-18 08:37:55 +0100

    Version 41 - saving the thesis latex

--- 3_implementations/1_Matlab_implementations/3_altered_sign_fn/eigenvalue_evaluation/eigenvalues_gmres.m
+++ 3_implementations/1_Matlab_implementations/3_altered_sign_fn/eigenvalue_evaluation/eigenvalues_gmres.m
@@ -7,14 +7,14 @@ gamma5hat = [speye(6), zeros(6,6); zeros(6,6), -speye(6)];
 Gamma5 = kron(speye(N/12),gamma5hat);
 A = Gamma5*A;
 
-m = 1000; %no.of critical eigenvalues
+m = 2000; %no.of critical eigenvalues
 
 % Setting up of the gmres solver
 tol = 1e-6;
 maxit = 200;
 
 % Generate the Ritz values (example for m=10, adjust as needed)
-m1 = 60; % Krylov subspace dimension
+m1 = 100; % Krylov subspace dimension
 
 %% Start the timing
 start = cputime;
@@ -24,7 +24,7 @@ precond1 = @(x) eval_precon_poly_inv(A, x, theta1, m1);
 Afun1 = @(x) gmres(A, x, [], tol, maxit, precond1);
 % Afun = @(x) gmres(A, x, [], tol, maxit);
 
-[~, D1] = eigs(Afun1, N, m, 'smallestabs');
+[Vm, D1] = eigs(Afun1, N, m, 'smallestabs'); % Computation time for 2000 eigenvalues of 8^4 lattice: 19480.8594 seconds 
 
 % End the timing
 finish = cputime;
@@ -34,13 +34,13 @@ disp(['Computation time: ', num2str(finish - start), ' seconds']);
 % save('eigen_values.mat', 'D1');
 %% Start the timing
 start = cputime;
-theta2 = ritz_value(A', m1);
+% theta2 = ritz_value(A', m1);
 precond2 = @(x) eval_precon_poly_inv(A', x, theta1, m1);
 
 Afun2 = @(x) gmres(A', x, [], tol, maxit, precond1);
 % Afun = @(x) gmres(A, x, [], tol, maxit);
 
-[Wm, D2] = eigs(Afun2, N, m+1, 'smallestabs');
+[Wm, D2] = eigs(Afun2, N, m+1, 'smallestabs'); % Computation time for 2000 eigenvalues of 8^4 lattice: 62166.7656 seconds
 
 % End the timing
 finish = cputime;
@@ -82,10 +82,10 @@ end
 % Remove the last column of Wm
 Wm = Wm(:, 1:m);
 
-D = diag(D1);
+% D = diag(D1);
 
 % Save eigenvalues and eigenvectors to file
-save('eigen_results.mat', 'Vm', 'Wm', 'D');
+save('critical_eigenvalues_2000_8to4.mat', 'Vm', 'Wm', 'D1');
 
 % % Output results
 % finish = cputime;
